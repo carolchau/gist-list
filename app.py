e58d6da2d54c3f1876f946d053ca46f6dafe0c7b
Carol Chau <carol.chau@macaulay.cuny.edu>
2016-11-12 23:22:45 -0500

    minor edits

--- app.py
+++ app.py
@@ -6,21 +6,22 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def gistlist():
 
     if request.method == "GET":
         return render_template("index.html")
-    #button = request.form['submit-username']
+
     if request.method == "POST":
         username = request.form['github-username']
         username = re.sub(r'\W+(?<!-)', '', username)
         r = requests.get('https://api.github.com/users/' + username + '/gists')
         gistList = json.loads(r.text or r.content)
+
         if not gistList or len(username) == 0:
             error_message = "Sorry, user does not exist or has no gists :("
             return render_template("index.html", error = error_message)
+
         return render_template("index.html", username=username, list=gistList)
 
 if __name__ == "__main__":
