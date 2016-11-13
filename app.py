c454e7acea940ef86642a247b4f29abfbc9e89f7
Carol Chau <carol.chau@macaulay.cuny.edu>
2016-11-12 21:46:18 -0500

    initial commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import json
 import re
-import dateutil.parser
-import datetime
+import os
 
 app = Flask(__name__)
 
@@ -25,5 +24,5 @@ def gistlist():
         return render_template("index.html", username=username, list=repoItem)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
